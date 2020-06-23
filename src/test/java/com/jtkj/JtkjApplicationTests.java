372fbe3be2ea85de287cc6112dc381c71c416ac5
yangmeng <603894673@qq.com>
2020-06-23 18:51:03 +0800

    生产版 commit

--- src/test/java/com/jtkj/JtkjApplicationTests.java
+++ src/test/java/com/jtkj/JtkjApplicationTests.java
@@ -21,16 +21,7 @@ public class JtkjApplicationTests {
 	@Test
 	public void test() {
 		try {
-			String ak = "4rVzy7tDym1fACImbvErcxkm";
-			String sk = "02zgbZTvplBs3ffszUInVZ0nZ7ecQd6w";
-			String access_token = getAuth(ak,sk);
-			System.out.println(access_token);
 
-			String path1 = "G:\\PhotoTestSystem\\photoTestSystem_back\\src\\main\\resources\\photo\\TIM20190312212233.jpg";
-			String path2 = "G:\\PhotoTestSystem\\photoTestSystem_back\\src\\main\\resources\\photo\\20190312213532.jpg";
-			String response = facemerge(path1,path2,access_token);
-
-			System.out.println(response);
 		} catch (Exception e) {
 			e.fillInStackTrace();
 		}
